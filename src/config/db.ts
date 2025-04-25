d97207bdb47526b787d84f858b425d231c71a897
JanetLai <show861031@gmail.com>
2025-04-25 08:40:27 +0800

    feat: 🎸 todo-list API GET CREATE DELETE EDIT

---
+++ src/config/db.ts
@@ -0,0 +1,26 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { Todo } from "../entities/Todo";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+console.log("✅ Connecting with config:", {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [Todo],
+  synchronize: true, // 開發階段可以 true，正式環境建議改成 false
+  logging: true,
+});
