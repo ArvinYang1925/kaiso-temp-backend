313ef21b8825f2ea03f431a8764fd4df763334e7
DinDin <peter779701@gmail.com>
2025-04-27 15:57:21 +0800

    feat: 🎸 add create todo method

--- src/controllers/todoController.ts
+++ src/controllers/todoController.ts
@@ -5,12 +5,27 @@ const todoRepository = AppDataSource.getRepository(Todo);
 
 module.exports = {
   getTodos,
+  createTodos,
 };
 
 async function getTodos(req: Request, res: Response, next: NextFunction): Promise<void> {
   try {
     const todos = await todoRepository.find();
     res.json({ status: "success", data: todos });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function createTodos(req: Request, res: Response, next: NextFunction): Promise<void> {
+  try {
+    let { title } = req.body;
+    if (!title) {
+      res.status(400).json({ status: "error", message: "Title is required" });
+    }
+    const newTodo = todoRepository.create({ title });
+    const saveTodo = await todoRepository.save(newTodo);
+    res.json({ status: "success", data: saveTodo });
   } catch (error) {
     next(error);
   }
